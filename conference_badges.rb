6a82e4de304e53d085eefa191542d63a446100f5
Andy Maskiell <maskiella@gmail.com>
2017-12-02 19:07:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,8 @@ def batch_badge_creator(names_array)
   return badges
 end
 
-def assign_rooms(room)
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room (#{index}+1)!"
 end
