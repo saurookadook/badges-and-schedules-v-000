625ea0e8b0a48cb09d4f93dbe2e9a2c6efa136e4
Andy Maskiell <maskiella@gmail.com>
2017-12-02 18:59:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+  badges = []
+  names_array.each do |name|
+    badges << badge_maker(name)
+  end
+  return badges
+end
+
+def assign_rooms(room)
+end
